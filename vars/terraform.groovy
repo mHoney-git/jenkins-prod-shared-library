d7cb2fe23739f8bdcbb5f8b5fff8fd6e424a1ccc
Honey Modi <Honey>
2021-06-11 18:24:00 -0400

    changes to shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -16,6 +16,12 @@ def call(Map config = [:]){
                 }
                 }
             }
+
+        stage ('installing apache') {
+            steps {
+                ansiblePlaybook credentialsId: 'AWS-SSH-KEY', disableHostKeyChecking: true, installation: 'Ansible', inventory: 'hosts.inv', playbook: 'apache.yml'
+              }
+        }
         }
     }
 }
