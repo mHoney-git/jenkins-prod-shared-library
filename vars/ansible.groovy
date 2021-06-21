af2b46ce3a35f92d2041c481fa34287bde3b059f
Honey Modi <Honey>
2021-06-21 17:22:47 -0400

    pushing updated ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -2,11 +2,17 @@ def call(Map config = [:]){
     pipeline {
     agent any 
     stages {
-        stage ("ansible playbook for deploying apache to it's respective server") {
+        stage ("shared pipeline for deploying apache to it's respective server") {
             steps { 
 		    ansiblePlaybook credentialsId: "AWS-SSH-KEY", disableHostKeyChecking: true, installation: "Ansible", inventory: "${config.apachehost}", playbook: "${config.apachefile}"
                 }
-            }    
+            }
+
+	stage ("shared pipeline for deploying tomcat to it's respective server") {
+            steps {
+               	    ansiblePlaybook credentialsId: "AWS-SSH-KEY", disableHostKeyChecking: true, installation: "Ansible", inventory: "${config.tomcathost}", playbook: "${config.tomcatfile}"
+              }
+        }    
         }
     }
 }
